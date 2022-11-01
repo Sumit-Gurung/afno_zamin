64b06c20df1b61009c6881193d52ada28d39b4e4
@maansum <sumandarji2479@gmail.com>
2022-11-01 17:45:37 +0545

    check validation

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:afnozamin/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,6 +12,10 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   String name = "";
+  final _formKey = GlobalKey<FormState>();
+   TextEditingController _nameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+ 
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -28,54 +34,76 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 25),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
-                    ),
-                    onChanged: (value) {
-                      name = value;
-                      setState(() {});
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                    child: Text("Login"),
-                  ),
-                  SizedBox(height: 35),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, MyRoutes.signupRoute);
-                    },
-                    child: Center(
-                      child: Container(
-                        // // height: 40,
-                        // width: 60,
-                        child: Text("SIGN UP",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15)),
-
-                        // alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
                       ),
+                       validator:MultiValidator([ RequiredValidator(errorText: "Required"),
+                       PatternValidator((r'^[a-z A-Z]+$'), errorText: 'Character only')]),
+                          
+                          controller: _nameController,
+                      
+                      onChanged: (value) {
+                        name = value;
+                        setState(() {});
+                      },
                     ),
-                  )
-                ],
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                       validator: MultiValidator([
+                            RequiredValidator(errorText: "Required"),
+                            MinLengthValidator(6,
+                                errorText:
+                                    "Password must contain atleast 6 characters"),
+   
+                   ]),
+                          controller: _passwordController,
+                    ),
+                    SizedBox(height: 35),
+                    ElevatedButton(
+                      onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                             
+                       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                                
+                              
+                            }
+                          },
+                      
+                      style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                      child: Text("Login"),
+                    ),
+                    SizedBox(height: 35),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, MyRoutes.signupRoute);
+                      },
+                      child: Center(
+                        child: Container(
+                          // // height: 40,
+                          // width: 60,
+                          child: Text("SIGN UP",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15)),
+              
+                          // alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.black))),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,5 +1,6 @@
 import 'package:afnozamin/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
@@ -10,90 +11,146 @@ class SignupPage extends StatefulWidget {
 
 class _SignupPageState extends State<SignupPage> {
   String name = "";
+   final _formKey = GlobalKey<FormState>();
+   TextEditingController _nameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _confirmController = TextEditingController();
+  
+  TextEditingController _phoneNumberController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+
+
+ 
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/Afno.png",
-              fit: BoxFit.cover,
-              height: 215,
-            ),
-            Text(
-              "Join Us",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
-            ),
-            SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Username",
-                      labelText: "Username",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Enter Password",
-                      labelText: "Password",
-                    ),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Confirm Password",
-                      labelText: "Confirm Password",
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter PhoneNumber",
-                      labelText: "PhoneNumber",
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter Email",
-                      labelText: "EmailAddress",
-                    ),
-                  ),
-                  SizedBox(height: 35),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
-                    },
-                    style: TextButton.styleFrom(minimumSize: Size(100, 40)),
-                    child: Text("SignUp"),
-                  ),
-                  SizedBox(height: 35),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, MyRoutes.loginRoute);
-                    },
-                    child: Center(
-                      child: Container(
-                        // // height: 40,
-                        // width: 60,
-                        child: Text("Log IN",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15)),
-
-                        // alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.black))),
-                      ),
-                    ),
-                  )
-                ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/Afno.png",
+                fit: BoxFit.cover,
+                height: 215,
               ),
-            ),
-          ],
+              Text(
+                "Join Us",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              ),
+              SizedBox(height: 25),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 35),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Username",
+                        labelText: "Username",
+                      ),
+                      validator:MultiValidator([ RequiredValidator(errorText: "Required"),
+                       PatternValidator((r'^[a-z A-Z]+$'), errorText: 'Character only')]),
+                          
+                          controller: _nameController,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                      validator: MultiValidator([
+                            RequiredValidator(errorText: "Required"),
+                            MinLengthValidator(6,
+                                errorText:
+                                    "Password must contain atleast 6 characters"),
+   
+                   ]),
+                          controller: _passwordController,
+
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      
+                      decoration: InputDecoration(
+                        hintText: "Confirm Password",
+                        labelText: "Confirm Password",
+                      ),
+                  //    
+                controller: _confirmController,
+                validator: (val){
+                              if(val==null) {
+                                return 'Empty';
+                              }
+                              if(val != _passwordController.text){
+                                   return 'Not Match';
+                              }
+                              return null;
+                              }
+                       
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter PhoneNumber",
+                        labelText: "PhoneNumber",
+                      ),
+                      validator: MultiValidator([
+                            RequiredValidator(errorText: "Required"),
+                           PatternValidator( r'^[0-9]{10}$', errorText: "only 10 digits allowed !")
+                          ]),
+                          controller: _phoneNumberController,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: "Enter Email",
+                        labelText: "EmailAddress",
+                      ),
+                      validator: MultiValidator([
+                            RequiredValidator(errorText: "Required"),
+                            EmailValidator(
+                                errorText:
+                                    "Please enter a valid email address"),
+                          ]),
+                          controller: _emailController,
+                    ),
+                    SizedBox(height: 35),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                             
+                       Navigator.pushNamed(context, MyRoutes.loginRoute);
+                                
+                              
+                            }
+                      },
+                      style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                      child: Text("SignUp"),
+                    ),
+                    SizedBox(height: 35),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, MyRoutes.loginRoute);
+                      },
+                      child: Center(
+                        child: Container(
+                          // // height: 40,
+                          // width: 60,
+                          child: Text("Log IN",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15)),
+        
+                          // alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                              border: Border(
+                                  bottom: BorderSide(color: Colors.black))),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ));
   }
 }
